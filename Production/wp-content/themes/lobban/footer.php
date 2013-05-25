91a35532757cc9dcb1f6e046b108201cc76bb52a
Andy Lobban <andy@lobban.org>
2013-05-25 17:32:27 +0100

    Fixed footer layout issue

--- Production/wp-content/themes/lobban/footer.php
+++ Production/wp-content/themes/lobban/footer.php
@@ -8,7 +8,8 @@
 		
 				<p>I'm a designer at <a href="http://stormid.com">Storm ID</a>. I run <a href="http://gerrylovesrecords.com" rel="me" class="url">Gerry Loves Records</a>, I help organise</span> <a href="http://refreshedinburgh.org">Refresh Edinburgh</a> and I started <a href="http://theshyretirers.com" rel="me" class="url">The Shy Retirers</a>. <a href="/about">There's more &rarr;</a></p>
 								
-				<ul class="wrapper profiles">
+				<ul class="profiles">
+				
 					<li><a href="/contact" class="ss-mail"><span>Email</span></a></li>
 					
 					<li><a href="http://twitter.com/nonimage" rel="me" class="url ss-twitter"><span>Twitter</span></a></li>
@@ -17,6 +18,7 @@
 					<li><a href="http://open.spotify.com/user/nonimage" rel="me" class="url ss-spotify"><span>Spotify</span></a></li>
 					<li><a href="http://www.goodreads.com/user/show/5679909-nonimage" rel="me" class="url ss-readmill"><span>Goodreads</span></a></li>
 					<li><a href="http://facebook.com/andylobban" rel="me" class="url ss-facebook"><span>Facebook</span></a></li> 	
+					
 				</ul>
 										
 			</div>
