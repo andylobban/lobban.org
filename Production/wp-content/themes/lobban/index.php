52f09bb09758066b5a300ed4180a525e9a9113ae
Andy Lobban <andy@lobban.org>
2013-05-25 17:19:55 +0100

    Sorted out hopme layout a bit, refactored CSS

--- Production/wp-content/themes/lobban/index.php
+++ Production/wp-content/themes/lobban/index.php
@@ -1,43 +1,42 @@
 <?php get_header(); ?>
+
+	<div class="clearfix wrapper entry" role="main">
 		
-	<section id="intro">
-	
-		<div class="clearfix wrapper">
-		
+		<section id="intro">
+										
+			<h1>Hi, I'm Andy.</h1>
+			
 			<figure>
-			
+		
 				<img src="<?php echo get_template_directory_uri(); ?>/images/common/andy-lobban.png" height="334" width="400" alt="photo of Andy Lobban" class="photo"/>
-	
+
 			</figure>
+						
+			<p>I make internet things and music things in <span class="adr"><span class="locality">Edinburgh</span>, <span class="country-name">Scotland</span></span>. I'm currently a designer at <a href="http://stormid.com">Storm ID</a>.</p>
 			
-			<div class="intro-text">
-							
-				<p>I make things, mostly for the internet or music in <span class="adr"><span class="locality">Edinburgh</span>, <span class="country-name">Scotland</span></span>. It keeps me off the streets and out of trouble.</p>
-				
-				<p>I'm currently a designer at <a href="http://stormid.com">Storm ID</a>. Previously I was a freelance designer under the name <a href="http://nonimage.com" rel="me" class="url">Nonimage</a>. I sometimes write things in a <a href="http://lobban.org/journal" rel="me" class="url">journal</a>. I run <a href="http://gerrylovesrecords.com" rel="me" class="url">Gerry Loves Records</a>, I help organise</span> <a href="http://refreshedinburgh.org">Refresh Edinburgh</a> and I started <a href="http://theshyretirers.com" rel="me" class="url">The Shy Retirers</a>.</p>
-				
-				<p><a href="/about">There's more &rarr;</a></p>
+			<p>I sometimes write things in a <a href="http://lobban.org/journal" rel="me" class="url">journal</a> and post on a <a href="http://film.lobban.org">photo blog</a>. I run <a href="http://gerrylovesrecords.com" rel="me" class="url">Gerry Loves Records</a>, I help organise</span> <a href="http://refreshedinburgh.org">Refresh Edinburgh</a> and I started <a href="http://theshyretirers.com" rel="me" class="url">The Shy Retirers</a>. Previously I freelanced under the name <a href="http://nonimage.com" rel="me" class="url">Nonimage</a>. </p>
 			
-			</div>
+			<p><a href="/about">There's more &rarr;</a></p>
+		
+		</section>
+		    			
+		<section class="clearfix profiles">
+					
+			<ul>
 			
-		</div>
-	
-	</section>
-	    			
-	<section id="profiles" class="clearfix">
+				<li><a href="/info" class="ss-mail"><span>Email</span></a></li>
 				
-		<ul class="wrapper profiles">
-			<li><a href="/info" class="ss-mail"><span>Email</span></a></li>
-			
-			<li><a href="http://twitter.com/nonimage" rel="me" class="url ss-twitter"><span>Twitter</span></a></li>
-			<li><a href="http://instagram.com/nonimage" rel="me" class="url ss-instagram"><span>Instagram</span></a></li>
-			<li><a href="http://pinboard.in/u:nonimage" rel="me" class="url ss-pinboard"><span>Pinboard</span></a></li>
-			<li><a href="http://open.spotify.com/user/nonimage" rel="me" class="url ss-spotify"><span>Spotify</span></a></li>
-			<li><a href="http://alpha.app.net/nonimage" rel="me" class="url ss-appdotnet"><span>App.net</span></a></li>
-			<li><a href="http://www.goodreads.com/user/show/5679909-nonimage" rel="me" class="url ss-readmill"><span>Goodreads</span></a></li>
-			<li><a href="http://facebook.com/andylobban" rel="me" class="url ss-facebook"><span>Facebook</span></a></li> 	
-		</ul>
+				<li><a href="http://twitter.com/nonimage" rel="me" class="url ss-twitter"><span>Twitter</span></a></li>
+				<li><a href="http://instagram.com/nonimage" rel="me" class="url ss-instagram"><span>Instagram</span></a></li>
+				<li><a href="http://pinboard.in/u:nonimage" rel="me" class="url ss-pinboard"><span>Pinboard</span></a></li>
+				<li><a href="http://open.spotify.com/user/nonimage" rel="me" class="url ss-spotify"><span>Spotify</span></a></li>
+				<li><a href="http://www.goodreads.com/user/show/5679909-nonimage" rel="me" class="url ss-readmill"><span>Goodreads</span></a></li>
+				<li><a href="http://facebook.com/andylobban" rel="me" class="url ss-facebook"><span>Facebook</span></a></li> 	
 				
-	</section>
+			</ul>
+					
+		</section>
+		
+	</div>
 
 <?php get_footer(); ?>
