e97f6fe7b30e4339dcfb12bca1f73a78fe7c5cc4
Andy Lobban <andy@stormid.com>
2015-07-10 16:25:30 +0100

    Tidies up

--- Production/wp-content/themes/lobban/index.php
+++ Production/wp-content/themes/lobban/index.php
@@ -3,133 +3,62 @@
 <main id="content" role="main">
 
     <section class="home__intro push--bottom" data-0="opacity: 1;" data-50p="opacity: 0;">
-
         <div class="wrapper clearfix">
-
             <div class="intro__info col small-12 medium-6 switch--medium">
-
                 <ul class="intro__projects">
                     <li>Designer at <a href="http://stormid.com">Storm ID</a></li>
                     <li>Co-owner of <a href="http://gerrylovesrecords.com/">Gerry Loves Records</a></li>
                     <li>Owner of <a href="http://secreteditions.com/">Secret Editions</a></li>
                     <li>Previously freelance as <a href="http://stormid.com">Nonimage</a></li>
                 </ul>
-
                 <ul class="intro__social clearfix">
                     <li><a class="icon-email" href="/contact"><span class="away">Email</span></a></li>
                     <li><a class="icon-twitter" href="https://twitter.com/nonimage"><span class="away">Twitter</span></a></li>
                     <li><a class="icon-instagram" href="https://instagram.com/nonimage/"><span class="away">Instagram</span></a></li>
                     <li><a class="icon-facebook" href="https://www.facebook.com/andylobban"><span class="away">Facebook</span></a></li>
                 </ul>
-
             </div>
-
         </div>
-
         <p class="intro__more" data-0="opacity: 1;" data-50p="opacity: 0;">
             <a href="#more"><i class="icon-down"></i>There's more</a>
         </p>
-
     </section>
 
     <div class="wrapper" id="more">
 
         <div class="home__section clearfix">
-
-            <img class="push--right" src="http://placehold.it/350x350" alt="" data-bottom-top="top: 20vh;" data-center-center="top: 0vh;" />
-
-            <div class="col small-12 medium-12 large-11 push--large-1 xlarge--10 push--xlarge-2">
-                <h2>Gerry Loves Records</h2>
+            <div class="home__project col small-12 medium-6">
+                <img src="http://placehold.it/350x350" alt="" /><h3>Gerry Loves Records</h3>
+                <p>Co-founder of a small record label specialising in quality vinyl records from mostly Scottish artists. <a href="http://gerrylovesrecords.com">gerrylovesrecords.com</a></p>
             </div>
-
-            <p class="col small-12 medium-8 push--medium-3 large-6 xlarge-6 push--xlarge-4"  data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;">Co-founder of a small record label specialising in quality vinyl records from mostly Scottish artists.</p>
-
-            <p class="col small-12 medium-11 push--medium-1 large-10 push--large-2 xlarge-5 push--xlarge-3" data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;"><a class="button" href="http://gerrylovesrecords.com">gerrylovesrecords.com</a></p>
-
-        </div>
-
-        <div class="home__section clearfix">
-
-            <img class="push--right" src="http://placehold.it/350x350" alt="" data-bottom-top="top: 20vh;" data-center-center="top: 0vh;" />
-
-            <div class="col small-12 medium-12 large-11 push--large-1 xlarge--10 push--xlarge-2">
-                <h2>Secret Editions</h2>
+            <div class="home__project col small-12 medium-6">
+                <img src="http://placehold.it/350x350" alt="" /><h3>Secret Editions</h3>
+                <p>Founder of a tiny record label specialising in cassette and book releases of experimental (pop) music. <a href="http://secreteditions.com">secreteditions.com</a></p>
             </div>
-
-            <p class="col small-12 medium-8 push--medium-3 large-6 xlarge-6 push--xlarge-4"  data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;">Founder of a tiny record label specialising in cassette and book releases of experimental (pop) music.</p>
-
-            <p class="col small-12 medium-11 push--medium-1 large-10 push--large-2 xlarge-5 push--xlarge-3" data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;"><a class="button" href="http://secreteditions.com">secreteditions.com</a></p>
-
-        </div>
-
-        <div class="home__section clearfix">
-
-            <img class="push--right" src="http://placehold.it/350x350" alt="" data-bottom-top="top: 20vh;" data-center-center="top: 0vh;" />
-
-            <div class="col small-12 medium-12 large-11 push--large-1 xlarge--10 push--xlarge-2">
-                <h2>Photoblog</h2>
+            <div class="home__project col small-12 medium-6">
+                <img src="http://placehold.it/350x350" alt="" /><h3>Photoblog</h3>
+                <p>A selection of erratic attempts at film photography. <a href="http://film.lobban.org">film.lobban.org</a></p>
             </div>
-
-            <p class="col small-12 medium-8 push--medium-3 large-6 xlarge-6 push--xlarge-4"  data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;">A selection of erratic attempts at film photography.</p>
-
-            <p class="col small-12 medium-11 push--medium-1 large-10 push--large-2 xlarge-5 push--xlarge-3" data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;"><a class="button" href="http://film.lobban.org">film.lobban.org</a></p>
-
-        </div>
-
-        <div class="home__section clearfix">
-
-            <img class="push--right" src="http://placehold.it/350x350" alt="" data-bottom-top="top: 20vh;" data-center-center="top: 0vh;" />
-
-            <div class="col small-12 medium-12 large-11 push--large-1 xlarge--10 push--xlarge-2">
-                <h2>Instant</h2>
+            <div class="home__project col small-12 medium-6">
+                <img src="http://placehold.it/350x350" alt="" /><h3>Instant</h3>
+                <p>A collection of every instant photo I've taken. <a href="http://instant.lobban.org">instant.lobban.org</a></p>
             </div>
-
-            <p class="col small-12 medium-8 push--medium-3 large-6 xlarge-6 push--xlarge-4"  data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;">A collection of (almost) every instant photo I've taken.</p>
-
-            <p class="col small-12 medium-11 push--medium-1 large-10 push--large-2 xlarge-5 push--xlarge-3" data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;"><a class="button" href="http://instant.lobban.org">instant.lobban.org</a></p>
-
-        </div>
-
-        <div class="home__section clearfix">
-
-            <img class="push--right" src="http://placehold.it/350x350" alt="" data-bottom-top="top: 20vh;" data-center-center="top: 0vh;" />
-
-            <div class="col small-12 medium-12 large-11 push--large-1 xlarge--10 push--xlarge-2">
-                <h2>The Shy Retirers</h2>
+            <div class="home__project col small-12 medium-6">
+                <img src="http://placehold.it/350x350" alt="" /><h3>The Shy Retirers</h3>
+                <p>An aggregator of Scottish music blogs, in Twitter, Tumblr and Facebook forms. <a href="http://theshyretirers.lobban.org">theshyretirers.lobban.org</a></p>
             </div>
-
-            <p class="col small-12 medium-8 push--medium-3 large-6 xlarge-6 push--xlarge-4"  data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;">An aggregator of Scottish music blogs, in Twitter, Tumblr and Facebook forms.</p>
-
-            <p class="col small-12 medium-11 push--medium-1 large-10 push--large-2 xlarge-5 push--xlarge-3" data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;"><a class="button" href="http://theshyretirers.lobban.org/">theshyretirers.lobban.org</a></p>
-
-        </div>
-
-        <div class="home__section clearfix">
-
-            <img class="push--right" src="http://placehold.it/350x350" alt="" data-bottom-top="top: 20vh;" data-center-center="top: 0vh;" />
-
-            <div class="col small-12 medium-12 large-11 push--large-1 xlarge--10 push--xlarge-2">
-                <h2>Sleeves Received Twitter</h2>
+            <div class="home__project col small-12 medium-6">
+                <img src="http://placehold.it/350x350" alt="" /><h3>Sleeves Received Twitter</h3>
+                <p>An unofficial Twitter feed for The Wire's <a href="http://thewiremagazine.tumblr.com/">Sleeves Received</a> blog. <a href="http://twitter.com/sleevesreceived">twitter.com/sleevesreceived</a></p>
+            </div>        
+            <div class="col small-12 medium-10 push--medium-1 large-8 push--large-2 xlarge-6 push--xlarge--3">
+                If you really want you can read about all my past projects, too.
             </div>
-
-            <p class="col small-12 medium-8 push--medium-3 large-6 xlarge-6 push--xlarge-4"  data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;">An unofficial Twitter feed for The Wire's <a href="http://thewiremagazine.tumblr.com/">Sleeves Received</a> blog.</p>
-
-            <p class="col small-12 medium-11 push--medium-1 large-10 push--large-2 xlarge-5 push--xlarge-3" data-bottom-top="margin-top: 20vh;" data-center-center="margin-top: 0vh;"><a class="button" href="https://twitter.com/sleevesreceived">twitter.com/sleevesreceived</a></p>
-
-        </div>
-
-        <div class="col small-12 medium-10 push--medium-1 large-8 push--large-2 xlarge-6 push--xlarge--3">
-
-            If you really want you can read about all my past projects, too.
-
         </div>
 
         <div class="home__section col small-12 medium-8 large-8 xlarge-9 clearfix">
-
             <h2>Posts</h2>
-
             <p>Things I've written</p>
-
             <article>
                 <a href="#">
                     <h3>Article title</h3>
@@ -148,61 +77,37 @@
                     <time>Last month</time>
                 </a>
             </article>
-
             <a class="button" href="/journal">More writing</a>
-
         </div>
 
         <div class="home__section col small-12 medium-4 large-4 xlarge-3 clearfix">
-
             <h2>Links</h2>
-
             <p>Things I've found</p>
-
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit <a href="#">url</a></p>
-
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit <a href="#">url</a></p>
-
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit <a href="#">url</a></p>
-
             <a class="button" href="/journal/category/links">More links</a>
-
         </div>
 
         <div class="home__section col small-12 medium-12 large-12 xlarge-12 clearfix">
-
             <h2>Instagram</h2>
-
             <p>Things I've seen</p>
-
             [photos x 4]
-
             <a class="button" href="http://instagram.com/nonimage">More Instagram</a>
-
         </div>
 
         <div class="home__section col small-12 medium-8 large-8 xlarge-9 clearfix">
-
             <h2>Music</h2>
-
             <p>Things I've listened to</p>
-
             [album covers x 4]
-
             <a class="button" href="#">Last.fm</a> <a class="button" href="#">Spotify</a> <a class="button" href="#">Discogs</a>
-
         </div>
 
         <div class="home__section col small-12 medium-4 large-4 xlarge-3 clearfix">
-
             <h2>Reading</h2>
-
             <p>Things I'm reading</p>
-
             [book cover] + title etc
-
             <a class="button" href="#">Goodreads</a>
-
         </div>
 
     </div>
