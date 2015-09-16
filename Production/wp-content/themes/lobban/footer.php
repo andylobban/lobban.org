5c901953f48944624a8c0d363f9640e96f7fbf0f
Andrew Lobban <nonimage@Andrews-MBP.home>
2015-09-16 13:00:12 +0100

    Loads fonts using Font Face Observer

--- Production/wp-content/themes/lobban/footer.php
+++ Production/wp-content/themes/lobban/footer.php
@@ -22,6 +22,27 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"><\/script>
 
 		<script>!window.jQuery && document.write('<script src="<?php echo get_template_directory_uri(); ?>/dist/js/libs/jquery-1.10.2.min.js"><\/script>')</script>
+
+		<script src="<?php echo get_template_directory_uri(); ?>/dist/js/libs/fontfaceobserver.js"></script>
+		<script>
+		    (function( w ){
+		    if( w.document.documentElement.className.indexOf( "fonts-loaded" ) > -1 ){
+		        return;
+		    }
+		    var font1 = new w.FontFaceObserver( "teimposheadlineblack", {
+		        weight: 400
+		    });
+		    var font2 = new w.FontFaceObserver( "karlaregular", {
+		        weight: 400,
+		        style: "italic"
+		    });
+		    w.Promise
+		        .all([font1.check(), font2.check()])
+		        .then(function(){
+		            w.document.documentElement.className += " fonts-loaded";
+		        });
+		    }( this ));
+		</script>
         
         <?php if(is_front_page()) {?>
         	<script src="<?php echo get_template_directory_uri(); ?>/dist/js/home.js"></script>
