a8a696d89f81d6146e7694637c444c9c610ad868
Andy Lobban <andy@lobban.org>
2015-09-16 23:07:34 +0100

    Removes jQuery

--- Production/wp-content/themes/lobban/footer.php
+++ Production/wp-content/themes/lobban/footer.php
@@ -19,10 +19,6 @@
 		<div class="edge edge--left"></div>
 		<div class="edge edge--right"></div>
 
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"><\/script>
-
-		<script>!window.jQuery && document.write('<script src="<?php echo get_template_directory_uri(); ?>/dist/js/libs/jquery-1.10.2.min.js"><\/script>')</script>
-
 		<script src="<?php echo get_template_directory_uri(); ?>/dist/js/libs/fontfaceobserver.js"></script>
 		<script src="<?php echo get_template_directory_uri(); ?>/dist/js/core.js"></script>
         
