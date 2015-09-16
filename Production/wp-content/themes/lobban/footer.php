aa19a6dbb04db6232db59c19edda4a07bdc81908
Andy Lobban <andy@lobban.org>
2015-09-16 13:21:12 +0100

    Fixes up all font loading, fixes alignment of <time>

--- Production/wp-content/themes/lobban/footer.php
+++ Production/wp-content/themes/lobban/footer.php
@@ -33,11 +33,16 @@
 		        weight: 400
 		    });
 		    var font2 = new w.FontFaceObserver( "karlaregular", {
-		        weight: 400,
-		        style: "italic"
+		        weight: 400
+		    });
+		    var font3 = new w.FontFaceObserver( "karlabold", {
+		        weight: 400
+		    });
+		    var font4 = new w.FontFaceObserver( "karlaitalic", {
+		        weight: 400
 		    });
 		    w.Promise
-		        .all([font1.check(), font2.check()])
+		        .all([font1.check(), font2.check(), font3.check(), font4.check()])
 		        .then(function(){
 		            w.document.documentElement.className += " fonts-loaded";
 		        });
