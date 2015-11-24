821bd5639c33df5056f7ff824536003d8eb70ae2
Andy Lobban <andy@stormid.com>
2015-11-24 15:03:33 +0000

    Removes Jetpack styles and scripts for head

--- Production/wp-content/themes/lobban/functions.php
+++ Production/wp-content/themes/lobban/functions.php
@@ -15,10 +15,12 @@ remove_action( 'wp_head', 'parent_post_rel_link');
 remove_action( 'wp_head', 'start_post_rel_link');
 remove_action( 'wp_head', 'adjacent_posts_rel_link');
 remove_action( 'wp_head', 'feed_links' );
-remove_action( 'wp_head', 'feed_links_extra', 3); // Remove category feeds
-remove_action( 'wp_head', 'feed_links', 2); // Remove Post and Comment Feeds
+remove_action( 'wp_head', 'feed_links_extra', 3);
+remove_action( 'wp_head', 'feed_links', 2);
 
 add_action( 'wp_print_styles', 'my_deregister_styles', 100 );
 function my_deregister_styles() {
 wp_deregister_style( 'cleaner-gallery' );
+wp_deregister_style('jetpack_css');
+wp_deregister_script('devicepx');
 }
