85212626ab46a1ecce90ff38d2a4078206b1c618
Andy Lobban <andy@lobban.org>
2016-10-27 00:00:50 +0100

    Improves placement and type sizing of home content

--- Production/wp-content/themes/lobban/page-home.php
+++ Production/wp-content/themes/lobban/page-home.php
@@ -7,11 +7,11 @@ Template Name: Home
 <?php get_header(); ?>
     <main id="content" role="main">
         <section class="home__cover wrapper clearfix">
-            <div class="col small-12 medium-12 large-12 xlarge-12">
+            <div class="col small-12 medium-9 large-8 push--large-2 xlarge-6">
                 <h1>
                     Andy Lobban
                 </h1>
-                <p><strong>Edinburgh, Scotland.</strong> Product designer at <a href="http://fanduel.com">FanDuel</a>. Previously at <a href="http://stormid.com">Storm ID</a>, <a href="http://nonimage.com">Nonimage</a>. Also <a href="http://gerrylovesrecords.com/">Gerry Loves Records</a>, <a href="http://secreteditions.com/">Secret Editions</a> and more.</p>
+                <p><span><strong>Edinburgh, Scotland.</strong> Product designer at <a href="http://fanduel.com">FanDuel</a>. </span><span>Previously at <a href="http://stormid.com">Storm ID</a>, <a href="http://nonimage.com">Nonimage</a>. </span><span>Also <a href="http://gerrylovesrecords.com/">Gerry Loves Records</a>, <a href="http://secreteditions.com/">Secret Editions</a> and more.</span></p>
             </div>
         </section>
     </main>
