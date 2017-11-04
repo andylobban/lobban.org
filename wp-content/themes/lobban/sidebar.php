bdc50db0d579651d96d41368c0e12fa3d24d4588
Andy Lobban <andy@lobban.org>
2017-11-04 08:14:44 +0000

    Styles article footer info

--- wp-content/themes/lobban/sidebar.php
+++ wp-content/themes/lobban/sidebar.php
@@ -1,9 +1,11 @@
-<div class="journal__extras col small-12 medium-10 large-7 xlarge-6 push--medium-2 push--large-2 push--xlarge-2 push--bottom push--top">
-	<h2 class="push--bottom push--top">More Journal</h2>
-	<ul class="push--bottom clearfix">
-		<?php wp_list_categories( 'title_li=' ); ?> 
-	</ul>
-	<ul class="push--bottom clearfix">
-		<?php wp_get_archives('type=yearly'); ?>
-	</ul>
+<div class="journal__extras grid">
+	<div class="journal__extras__wrapper">
+		<h2>More Journal</h2>
+		<ul>
+			<?php wp_list_categories( 'title_li=' ); ?> 
+		</ul>
+		<ul>
+			<?php wp_get_archives('type=yearly'); ?>
+		</ul>
+	</div>
 </div>
