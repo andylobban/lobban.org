bd6854b94751c1af0cdac1aa706ceb40538beed2
Andy Lobban <andy@lobban.org>
2017-11-07 22:10:32 +0000

    Fixes up 404

--- wp-content/themes/lobban/404.php
+++ wp-content/themes/lobban/404.php
@@ -1,14 +1,16 @@
 <?php get_header(); ?>
 
-<main class="page--info article wrapper push--bottom clearfix" id="content" role="main">
-    <h1 class="page__header col small-12 medium-12 large-12 xlarge-12 modules__header">
-       	404 - Not Found
-    </h1>
-    <div class="article__content">
-        <section class="col small-12 medium-6 large-6 xlarge-6">
-			<p>Sorry, we couldn't find what you were looking for. Don't worry, it's probably my fault. Maybe you probably followed a broken link or I've moved something.</p>
-			<p><a class="button" href="/">Go home</a></p>
-        </section>
+<main id="content" role="main">
+    <div class="grid">
+        <div class="article__body">
+        	<h1 class="headline page__header">
+                Not Found
+            </h1>
+            <div class="article__content body-copy">
+                <p>Sorry, we couldn't find what you were looking for. Don't worry, it's probably my fault. Maybe you probably followed a broken link or I've moved something.</p>
+				<p><a class="button" href="/">Go home</a></p>
+            </div>
+        </div>
     </div>
 </main>
 
