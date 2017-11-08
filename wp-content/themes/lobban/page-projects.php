cbfb2f78c4847cca89bbb88f1a07e4623d0ed336
Andy Lobban <andy@lobban.org>
2017-11-08 09:17:23 +0000

    Adds Projects template, tweaks Info

---
+++ wp-content/themes/lobban/page-projects.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Projects
+*/
+?>
+
+<?php get_header(); ?>
+
+<main class="page--work" id="content" role="main">
+    <?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+        <div class="grid">
+            <div class="article__body">
+                <h1 class="headline page__header">
+                    <?php the_title(); ?>
+                </h1>
+                <div class="article__content body-copy">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    <?php endwhile; ?>
+    <?php endif; ?>
+</main>
+
+<?php get_footer(); ?>
