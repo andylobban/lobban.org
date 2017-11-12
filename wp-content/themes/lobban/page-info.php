cf3f9665f4a8884cc363bac89fb6d404c0d5114c
Andy Lobban <andy@lobban.org>
2017-11-12 20:06:11 +0000

    Improves Info content

--- wp-content/themes/lobban/page-info.php
+++ wp-content/themes/lobban/page-info.php
@@ -28,7 +28,7 @@ Template Name: Info
         </div>
         <div class="grid">
             <section class="info__section body-copy">
-                <h2 class="headline--sub">Current personal projects</h2>
+                <h2 class="headline--sub">Personal projects</h2>
                 <ul>
                     <li>
                         <h3><a href="http://gerrylovesrecords.com" rel="me">Gerry Loves Records</a></h3>
@@ -61,19 +61,19 @@ Template Name: Info
                 <ul>
                     <li>
                         <h3>2016-present: Principal product designer at <a href="http://fanduel.com" rel="me">FanDuel</a></h3>
-                        <p></p>
+                        <p>Design lead, plus line management and design team direction.</p>
                     </li>
                     <li>
                         <h3>2015-2016: Product designer at <a href="http://fanduel.com" rel="me">FanDuel</a></h3>
-                        <p></p>
+                        <p>Digital product design for Android, iOS and web apps.</p>
                     </li>
                     <li>
                         <h3>2010-2015: Designer at <a href="http://stormid.com" rel="me">Storm ID</a></h3>
-                        <p>Digital design and front end development (HTML and CSS with a little JS) with a bit of UX design thrown in.</p>
+                        <p>Digital design and frontend development with a bit of UX.</p>
                     </li>
                     <li>
                         <h3>2007-2010: Freelance designer as <a href="http://nonimage.com" rel="me">Nonimage</a></h3>
-                        <p>Freelance web and print design.</p>
+                        <p>Freelance digital and print design.</p>
                     </li>
                     <li>
                         <h3>2004-2007: Designer at What's On Publications</h3>
@@ -82,6 +82,19 @@ Template Name: Info
                     <li>
                         <h3>2000-2004: BA Hons in Graphic Communcation at Napier University, Edinburgh</h3>
                         <p>Design for print and web, pre-production, printing, finishing, publishing, marketing.</p>
+                    </li>
+                </ul>
+            </section>
+            <section class="info__section body-copy">
+                <h2 class="headline--sub">Chat</h2>
+                <ul class="modules clearfix">
+                    <li>
+                        <h3><a title="Andy Lobban interview on Clear-Minded Creative" href="http://www.clearmindedcreative.com/andy-lobban/">Interview on Clear-Minded Creative</a></h3>
+                        <p>Milo McLaughlin asked me about design, Gerry Loves Records and Off The Beaten Tracks, among other things.</p>
+                    </li>
+                    <li>
+                        <h3>Gerry Loves Records interviews</h3>
+                        <p>I’ve done interviews for the record label with the likes of <a title="Andy Lobban interview with The List" href="http://www.list.co.uk/article/31515-labels-of-love-gerry-loves-records/">The List</a>, <a title="Andy Lobban interview with The Scotsman" href="http://radar.scotsman.com/viewpost.aspx?id=81">The Scotsman</a>, <a title="Andy Lobban interview with Peenko" href="http://www.peenko.co.uk/2010/09/scottish-diy-labels-8-gerry-loves.html">Peenko</a>, <a title="Andy Lobban interview with The Bomber Jacket" href="http://thebomberjacket.com/2012/06/26/gerry-loves-records-and-we-like-gerry-a-lot/">The Bomber Jacket</a>, and&nbsp;<a title="Andy Lobban interview at Argos Barks" href="http://www.argosbarks.com/post/655684670/interview-gerry-loves-records">Argos Barks</a>.</p>
                     </li>
                 </ul>
             </section>
@@ -117,21 +130,8 @@ Template Name: Info
                         <p>A repository of information about SXSW, before there were hundreds. Tips, tricks, info for first-timers, highlights, etc.&nbsp;The website is gone but the&nbsp;<a title="SXSW is Easy" href="https://twitter.com/SXSWisEASY">Twitter feed</a>&nbsp;is still there. We ran an experiment, giving out 6 disposable cameras with our contact details on them, asking people to take a couple fo photos and pass them on. <a title="The great SXSWisEasy camera experiment" href="http://www.flickr.com/photos/stalelife/sets/72157617339250745/">Only one</a> ever made it back. With <a title="Kurt Pennypacker" href="https://www.linkedin.com/pub/kurt-pennypacker/1/709/371">Kurt</a>, <a title="Joe Doyle" href="http://joedoyle.com/">Joe</a> and <a title="Connor Turner" href="http://www.ctoverdrive.ca/">Connor</a>.</p>
                     </li>
                     <li>
-                        <h3>2003-present:&nbsp;<a title="Nopesport" href="http://www.nopesport.com/">Nopesport</a></h3>
+                        <h3>2003-2013:&nbsp;<a title="Nopesport" href="http://www.nopesport.com/">Nopesport</a></h3>
                         <p>The first proper online community for orienteering in the UK. Forums, news, photos and more. Cofounded with&nbsp;<a title="Alastair Brunton" href="http://www.simplyexcited.co.uk/">Alastair Brunton</a>. The site is still active but I'm no longer involved much.</p>
-                    </li>
-                </ul>
-            </section>
-            <section class="info__section body-copy">
-                <h2 class="headline--sub">Chat</h2>
-                <ul class="modules clearfix">
-                    <li>
-                        <h3><a title="Andy Lobban interview on Clear-Minded Creative" href="http://www.clearmindedcreative.com/andy-lobban/">Interview on Clear-Minded Creative</a></h3>
-                        <p>Milo McLaughlin asked me about design, Gerry Loves Records and Off The Beaten Tracks, among other things.</p>
-                    </li>
-                    <li>
-                        <h3>Gerry Loves Records interviews</h3>
-                        <p>I’ve done interviews for the record label with the likes of <a title="Andy Lobban interview with The List" href="http://www.list.co.uk/article/31515-labels-of-love-gerry-loves-records/">The List</a>, <a title="Andy Lobban interview with The Scotsman" href="http://radar.scotsman.com/viewpost.aspx?id=81">The Scotsman</a>, <a title="Andy Lobban interview with Peenko" href="http://www.peenko.co.uk/2010/09/scottish-diy-labels-8-gerry-loves.html">Peenko</a>, <a title="Andy Lobban interview with The Bomber Jacket" href="http://thebomberjacket.com/2012/06/26/gerry-loves-records-and-we-like-gerry-a-lot/">The Bomber Jacket</a>, and&nbsp;<a title="Andy Lobban interview at Argos Barks" href="http://www.argosbarks.com/post/655684670/interview-gerry-loves-records">Argos Barks</a>.</p>
                     </li>
                 </ul>
             </section>
