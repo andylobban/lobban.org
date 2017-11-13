8b396c86735338b31027096f4d3921bee5eb0bb3
Andy Lobban <andy@lobban.org>
2017-11-13 22:31:35 +0000

    Fixes More items layout

--- wp-content/themes/lobban/sidebar.php
+++ wp-content/themes/lobban/sidebar.php
@@ -1,10 +1,10 @@
 <div class="journal__extras grid wrapper">
 	<div class="journal__extras__wrapper">
 		<h2 class="sub-headline">More</h2>
-		<ul>
+		<ul class="category-list">
 			<?php wp_list_categories( 'title_li=' ); ?> 
 		</ul>
-		<ul>
+		<ul class="year-list grid grid--small">
 			<?php wp_get_archives('type=yearly'); ?>
 		</ul>
 	</div>
