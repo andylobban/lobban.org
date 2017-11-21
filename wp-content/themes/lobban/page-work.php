f8375e77d95aa839da32b91b26144fab065e37f8
Andy Lobban <andy@lobban.org>
2017-11-21 23:00:50 +0000

    Adds images and dates

--- wp-content/themes/lobban/page-work.php
+++ wp-content/themes/lobban/page-work.php
@@ -19,7 +19,7 @@ Template Name: Work
                     <h2 class="work__section__logo">
                     <img src="<?php echo get_template_directory_uri(); ?>/dist/images/fanduel-horizontal-logo.svg" alt="FanDuel" />
                 </h2>
-                    <h3 class="work__section__title">Principal product designer</h3>
+                    <h3 class="work__section__title">Principal product designer &nbsp;&nbsp;<em>2015&ndash;now</em></h3>
                     <p>Digital product design for native and web apps. FanDuel's mission is to make sports more exciting, through our fantasy sports apps in the US market. It's one of Scotland's two <a href="https://en.wikipedia.org/wiki/Unicorn_(finance)">unicorn</a> companies. I lead design work for one of our products, as well as working with the other principals to lead the wider design team.</p>
                     <p><a href="#" class="button">Case study</a></p>
                 </div>
@@ -33,8 +33,12 @@ Template Name: Work
                     <h2 class="work__section__logo">
                         <img src="<?php echo get_template_directory_uri(); ?>/dist/images/storm-id-logo.svg" alt="Storm ID" />
                     </h2>
-                    <h3 class="work__section__title">Digital designer</h3>
+                    <h3 class="work__section__title">Digital designer &nbsp;&nbsp;<em>2010&ndash;2015</em></h3>
                     <p>Design for web and native apps for one of Scotland's leading agencies and consultancies. Wireframing, concepts, HTML/CSS development. I worked on project for a range of clients including The Scottish Government, ESPC, The R&amp;A, The City of Edinburgh Council, Standard Life, Young Scot, DC Thomson, Tramway, and ScotCen.</p>
+                </div>
+                <div class="work__section__support">
+                    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/stormid.jpg" alt="Andy working at Storm ID" />
+                    <div class="work__section__support__shade"></div>
                 </div>
             </div>
         </div>
