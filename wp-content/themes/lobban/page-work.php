acde286b3e25191169a277659acb12225f77bd8e
Andy Lobban <andy@lobban.org>
2017-11-23 21:19:28 +0000

    Adds Nonimage section

--- wp-content/themes/lobban/page-work.php
+++ wp-content/themes/lobban/page-work.php
@@ -34,10 +34,23 @@ Template Name: Work
                         <img src="<?php echo get_template_directory_uri(); ?>/dist/images/storm-id-logo.svg" alt="Storm ID" />
                     </h2>
                     <h3 class="work__section__title">Digital designer &nbsp;&nbsp;<em>2010&ndash;2015</em></h3>
-                    <p>Design for web and native apps for one of Scotland's leading agencies and consultancies. Wireframing, concepts, HTML/CSS development. I worked on project for a range of clients including The Scottish Government, ESPC, The R&amp;A, The City of Edinburgh Council, Standard Life, Young Scot, DC Thomson, Tramway, and ScotCen.</p>
+                    <p>Design for web and native apps for one of Scotland's leading agencies and consultancies. Wireframing, concepts, HTML/CSS development. I worked on projects for a range of clients including The Scottish Government, ESPC, The R&amp;A, The City of Edinburgh Council, Standard Life, Young Scot, DC Thomson, Tramway, and ScotCen.</p>
                 </div>
                 <div class="work__section__support">
                     <img src="<?php echo get_template_directory_uri(); ?>/dist/images/stormid.jpg" alt="Andy working at Storm ID" />
+                    <div class="work__section__support__shade"></div>
+                </div>
+            </div>
+            <div class="work__section work__section--nonimage grid grid--small">
+                <div class="work__section__content">
+                    <h2 class="work__section__logo">
+                        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/nonimage-logo.svg" alt="Nonimage" />
+                    </h2>
+                    <h3 class="work__section__title">Freelance designer &nbsp;&nbsp;<em>2007&ndash;2010</em></h3>
+                    <p>Web, print and brand design, HTML/CSS and Wordpress development. Clients included Red Bull and The Leith Agency, Southern Records, and Alzheimer Scotland.</p>
+                </div>
+                <div class="work__section__support">
+                    <img src="<?php echo get_template_directory_uri(); ?>/dist/images/nonimage.jpg" alt="Image from work done by Nonimage" />
                     <div class="work__section__support__shade"></div>
                 </div>
             </div>
