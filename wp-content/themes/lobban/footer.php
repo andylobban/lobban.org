f706c96eacbac5d74ebfec2758fbd2e05c6feab7
Andy Lobban <andrew.lobban@fanduel.com>
2017-12-04 14:01:26 +0000

    Removes old reference to core.js

--- wp-content/themes/lobban/footer.php
+++ wp-content/themes/lobban/footer.php
@@ -6,8 +6,6 @@
 			</footer>
 		<?php }?>
 
-		<script src="<?php echo get_template_directory_uri(); ?>/dist/js/core.js"></script>
-
         <script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
