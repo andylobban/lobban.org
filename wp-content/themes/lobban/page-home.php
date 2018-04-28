1ec9268120cbfcceaa6577aac7f3eb76a7cb3b79
Andy Lobban <andy@lobban.org>
2018-04-28 15:33:42 +0100

    Reworks info section to paras

--- wp-content/themes/lobban/page-home.php
+++ wp-content/themes/lobban/page-home.php
@@ -9,12 +9,10 @@ Template Name: Home
         <div class="home__info__wrapper">
             <h1 class="home__info__name">Andy Lobban</h1>
             <p class="home__info__location">Edinburgh, Scotland</p>
-            <ul class="home__info__intro">
-                <li>Design Director at <a href="https://fanduel.com">FanDuel</a></li>
-                <li>Co-owner of <a href="http://gerrylovesrecords.com">Gerry Loves Records</a></li>
-                <li>Owner of <a href="http://secreteditions.com">Secret Editions</a></li>
-                <li>Some <a href="/info">other stuff</a></li>
-            </ul>
+            <div class="home__info__intro">
+                <p>Design Director at <a href="https://fanduel.com">FanDuel</a>, co-owner of <a href="http://gerrylovesrecords.com">Gerry Loves Records</a>, owner of <a href="http://secreteditions.com">Secret Editions</a>. </p>
+                <p>You can see some <a href="/work">work</a>, read some <a href="/journal">posts</a>, or get more <a href="/info">info</a>.</p>
+            </div>
             <?php include TEMPLATEPATH . '/social-links.php'; ?>
         </div>
     </main>
